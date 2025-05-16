387a3dcfac594d6eb2c05144357fd175be8a1dc3
Yukiko-Shimoyama <sy2071023@keio.jp>
2025-05-16 06:55:53 +0000

    7 hit計算完成

--- main.c
+++ main.c
@@ -6,8 +6,10 @@
 #define BUFSIZE 1024 //ファイルから読み込む一行の最大文字数
 #define MAX_SEQ_NUM 30 //一つの転写因子に対して与えられる結合部位配列の最大数
 #define MAX_GENE_NUM 8 /*与えられるプロモータ領域の最大遺伝子数*/
+#define NUC_NUM 4 
 
 char g_motif[MAX_SEQ_NUM][BUFSIZE]; //転写因子の結合部位配列を保存する配列
+
 
 struct promoter{
   char name[BUFSIZE];
@@ -65,47 +67,18 @@ int read_promoter(char *filename){
 
 int main(int argc, char* argv[]){
   int seq_num = read_multi_seq(argv[1]); //１番目の引数で指定した転写因子の複数の結合部位配列を読み込む
-
   int gene_num = read_promoter(argv[2]);  //２番目の引数で指定した遺伝子のプロモータ領域を読み込む
   make_frequency_matrix(seq_num);
 
   return 0;
 }
 
-
-/*void f(char *filename){
-    for(int i=0; i<seq_num; i++){
-        for(j=0; j<motif_len; j++){
-            char bace=g_motif[i][j];
-            switch(bace[i]){
-                case 'A' : matrix[A][i]++; break;
-                case 'C' : matrix[C][i]++; break;
-                case 'G' : matrix[G][i]++; break;
-                case 'T' : matrix[Y][i]++; break;
-                default : fprint(stderr, "unknown char %c/n", bace);
-            }
-    }
-    }
-    
-    for (i=0; i<4; k++){
-
-        for(int j=0; j<MAX_GENE_NUM; j++){
-            printf(matrix[j][i]);}
-        printf("\n");
-
-        }
-
-    }
-  }   
-*/
-
-
 void make_frequency_matrix(int motif_num)
 {
   if(motif_num == 0) return;
 
   int motif_len = strlen(g_motif[0]);
-  int matrix[4][motif_len]; // A=0, T=1, G=2, C=3
+  int matrix[NUC_NUM][motif_len]; // A=0, T=1, G=2, C=3
 
   // 初期化
   for(int i = 0; i < 4; i++){
@@ -115,7 +88,6 @@ void make_frequency_matrix(int motif_num)
   }
 
   // カウント
-
   for(int i = 0; i < motif_num; i++){
     for(int j = 0; j < motif_len; j++){
       char base = g_motif[i][j];
@@ -124,18 +96,16 @@ void make_frequency_matrix(int motif_num)
         case 'T': matrix[1][j]++; break;
         case 'G': matrix[2][j]++; break;
         case 'C': matrix[3][j]++; break;
-      
       } 
     }
   }
 
   // 出力
-
+  printf("  A T G C\n");
   for(int j = 0; j < motif_len; j++)
   {
     printf("%d %d %d %d %d\n", j + 1, matrix[0][j], matrix[1][j], matrix[2][j], matrix[3][j]);
   }
-
 
   float Pi[4][motif_len]; // A=0, T=1, G=2, C=3
   int bunbo=18;
@@ -151,14 +121,11 @@ void make_frequency_matrix(int motif_num)
     for(int j = 0; j < motif_len; j++){
       float bunshi=(float)matrix[i][j]+1;
       Pi[i][j]=bunshi/bunbo;
-      printf("%f ",Pi[i][j]);
     } 
-    printf("\n");
   }
 
-
   int Q[4];
-  Q[0]=7519429; Q[1]=4637676; Q[2]=4637676; Q[3]=7519429;
+  Q[0]=7519429; Q[1]=7519429; Q[2]=4637676; Q[3]=4637676;
   int totalQ=0;
   for(int i=0;i<4;i++){totalQ+=Q[i];}
   float q[4];
@@ -167,7 +134,7 @@ void make_frequency_matrix(int motif_num)
   }
   
   printf("\nオッズスコア\n");
-  double s[4][motif_len];
+  double s[NUC_NUM][motif_len];
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < motif_len; j++){
       double x=Pi[i][j]/q[i];
@@ -176,4 +143,21 @@ void make_frequency_matrix(int motif_num)
     } 
       printf("\n");
   }
-}
+
+int promoter_size=strlen(g_pro[0].seq);
+int motif_size=strlen(g_motif[0]);
+double hit;
+
+for(int i=0; i<MAX_GENE_NUM; i++){
+  printf("\npro: %s\n", g_pro[i].name);
+  for(int j=0; j<promoter_size-motif_size+1; j++){
+    hit=0;
+    for(int k=0; k<motif_size; k++){
+      if(g_pro[i].seq[j+k]=='A'){hit += s[0][k];}
+      if(g_pro[i].seq[j+k]=='T'){hit += s[1][k];}
+      if(g_pro[i].seq[j+k]=='G'){hit += s[2][k];}
+      if(g_pro[i].seq[j+k]=='C'){hit += s[3][k];}}
+      if(hit > 5){printf("No.%d score=%f\n", j+1, hit);}
+      }
+    }
+  }
